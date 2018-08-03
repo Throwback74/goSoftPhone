a66e6afe7159d3b394412f7b7f7aeac69499ff4d
Throwback74 <corey.slade@gmail.com>
2018-08-03 07:07:56 -0700

    update

--- public/RepoStuff/assets/js/dialer.js
+++ public/RepoStuff/assets/js/dialer.js
@@ -1,9 +1,4 @@
-// var input=document.querySelectorAll("#phoneNumField input")[0];
-// // var reset=document.querySelectorAll(".reset button")[0];
-// var btn = document.querySelectorAll(".column button");
 
-// var inputNum = $("#phoneNumField").val();
-// var btnVal = $(".numBtn").text();
 // console.log(btnVal); - pattern="-?[0-9]*(\.[0-9]+)?"
 var btnVal;
 var btnInput = "";
@@ -22,18 +17,6 @@ $(".numBtn").click(function() {
     $("#phoneNumField").val(inputArr[inputArr.length-1]);
 });
 
-
-// for(var i=0; i<btn.length;i++){
-//    btn[i].addEventListener(`click`,function(){
-
-//        var num1=input.value;
-//        var num2=this.childNodes[0].nodeValue;
-       
-//        var num=num1+num2;
-//        input.value=num;  
-
-//    });
-// }
 
 
 
